631959fe03cd8a4dae849d2f5598e708aaabb890
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 17:13:21 +0800

    counts even number and print it

--- intermediate_arithmetic.py
+++ intermediate_arithmetic.py
@@ -46,7 +46,7 @@ while start == "y":
         num2 = int(input("2nd number? "))
         print(f"{num1 % num2}")
         
-# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+    # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
     if program == 6:
         num1 = int(input("1st number? "))
@@ -60,3 +60,43 @@ while start == "y":
         num9 = int(input("9th number? "))
         num10 = int(input("10th number? "))
         print(f"{num1 - (num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10)}")
+        
+    # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+    if program == 7:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        num3 = int(input("3rd number? "))
+        num4 = int(input("4th number? "))
+        num5 = int(input("5th number? "))
+        num6 = int(input("6th number? "))
+        num7 = int(input("7th number? "))
+        num8 = int(input("8th number? "))
+        num9 = int(input("9th number? "))
+        num10 = int(input("10th number? "))
+        
+    # Checks if number is odd
+    
+        even_counter = 0
+        if num1 % 2 == 0:
+            even_counter += 1
+        if num2 % 2 == 0:
+            even_counter += 1
+        if num3 % 2 == 0:
+            even_counter += 1    
+        if num4 % 2 == 0:
+            even_counter += 1
+        if num5 % 2 == 0:
+            even_counter += 1
+        if num6 % 2 == 0:
+            even_counter += 1
+        if num7 % 2 == 0:
+            even_counter += 1
+        if num8 % 2 == 0:
+            even_counter += 1
+        if num9 % 2 == 0:
+            even_counter += 1
+        if num10 % 2 == 0:
+            even_counter += 1
+            
+        print(even_counter)
