545da954d4a92f601b76b664926fd4a933790324
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 20:54:57 +0800

    only duplicates show

---
+++ practice_python_operations/input_invalidations.py
@@ -0,0 +1,22 @@
+# creating a loop
+
+start = "y"
+while start == "y":
+    
+# ask them the choose program
+
+    program = int(input("Program(1-5)? "))
+
+    # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+    if program == 1:
+        num_counter = 0
+        numbers = []
+        for num in range(10):
+            num_counter += 1
+            num = int(input(f"Number #{num_counter}? "))
+            numbers.append(num)
+
+        # Make the numbers with copy show
+        duplicate_numbers = [num for num in numbers if numbers.count(num) > 1]
+        print (duplicate_numbers)
