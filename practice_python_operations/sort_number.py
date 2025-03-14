fd9ee4cd88bd51c3085c2802162cb820913b31ec
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 19:55:13 +0800

    remove duplicates on list, print numbers

--- practice_python_operations/sort_number.py
+++ practice_python_operations/sort_number.py
@@ -20,3 +20,23 @@ while start == "y":
         # Make individual numbers unique to 1 copy
         unique_numbers = [num for num in numbers if numbers.count(num) == 1]
         print (unique_numbers)        
+
+    # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+    
+    if program == 2:
+        num_counter = 0
+        numbers = []
+        for num in range(10):
+            num_counter += 1
+            num = int(input(f"Number #{num_counter}? "))
+            numbers.append(num)
+
+        unique_numbers = []
+
+        # remove duplicates
+
+        for num in numbers:
+            if num not in unique_numbers:
+                unique_numbers.append(num)
+
+        print(unique_numbers)
