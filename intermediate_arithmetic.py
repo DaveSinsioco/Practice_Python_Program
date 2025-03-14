61fd3a674774e1cea9afa411497fc9dbf6c39312
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 17:06:33 +0800

    print smaller number

--- intermediate_arithmetic.py
+++ intermediate_arithmetic.py
@@ -6,3 +6,13 @@ while start == "y":
 # asking which program
 
     program = int(input("Program(1-10)? "))
+        
+    # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+    if program == 1:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        if num1 < num2:
+            print(num1)
+        if num1 > num2:
+            print(num2)    
