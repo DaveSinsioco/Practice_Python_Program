e9db3e42f6a962b81e573fb720e8707b0aeab7db
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 16:49:31 +0800

    print equal if equal num

--- practice_python_operations/basic_arithmetic.py
+++ practice_python_operations/basic_arithmetic.py
@@ -15,3 +15,11 @@ while start == "y":
             print(num1)
         if num1 < num2:
             print(num2)
+
+    # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+    if program == 2:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        if num1 == num2:
+            print("Equal")        
