6126f5acacd56216ce13ca6dd948bbaf8a7b3a1c
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 16:51:25 +0800

    print andwer on basic math operations

--- practice_python_operations/basic_arithmetic.py
+++ practice_python_operations/basic_arithmetic.py
@@ -23,3 +23,31 @@ while start == "y":
         num2 = int(input("2nd number? "))
         if num1 == num2:
             print("Equal")        
+
+    # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+    if program == 3:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        print(f"{num1 + num2}")
+
+    # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+    if program == 4:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        print(f"{num1 * num2}")
+
+    # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+    if program == 5:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        print(f"{float(num1 / num2)}")
+        
+    # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+    if program == 6:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        print(f"{num1 ** num2}")        
