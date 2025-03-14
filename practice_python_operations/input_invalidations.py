874b041595708c4caec9cab42bd4629c13bbb0ab
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 21:06:58 +0800

    uses key to find the number with max count

--- practice_python_operations/input_invalidations.py
+++ practice_python_operations/input_invalidations.py
@@ -20,3 +20,20 @@ while start == "y":
         # Make the numbers with copy show
         duplicate_numbers = [num for num in numbers if numbers.count(num) > 1]
         print (duplicate_numbers)
+
+    # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+    if program == 2:
+        numbers = []
+        while True:
+            try:
+                while True:
+                    num = int(input("Number? "))
+                    numbers.append(num)
+            except ValueError:
+                break
+
+        # Display the number with the most number of duplicate.
+
+        duplicate_numbers = [num for num in numbers if numbers.count(num) > 1]
+        print (max(set(duplicate_numbers), key=duplicate_numbers.count))
