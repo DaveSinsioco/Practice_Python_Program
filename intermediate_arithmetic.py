887134677010175b64f64382d526dadf46dee324
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 17:14:50 +0800

    in number 0 to 100, if number is odd it will not be printed

--- intermediate_arithmetic.py
+++ intermediate_arithmetic.py
@@ -100,3 +100,12 @@ while start == "y":
             even_counter += 1
             
         print(even_counter)
+
+    # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+    if program == 8:
+        number = 0
+        while number <= 100:
+            if number % 2 != 0:
+                print(number)
+            number += 1
