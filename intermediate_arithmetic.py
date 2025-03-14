87cbbdcfbaed22ba945b94284e555ce8d6ef2fb1
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 17:07:35 +0800

    print not equal if not equal

--- intermediate_arithmetic.py
+++ intermediate_arithmetic.py
@@ -15,4 +15,12 @@ while start == "y":
         if num1 < num2:
             print(num1)
         if num1 > num2:
-            print(num2)    
+            print(num2) 
+
+    # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+    if program == 2:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
+        if num1 != num2:
+            print("Not Equal")
