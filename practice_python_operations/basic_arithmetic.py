c25406509b133454e0b513cf63103f46518fd6bc
DaveSinsioco <sinciocodave@gmail.com>
2025-03-14 16:45:31 +0800

    2 number input

--- practice_python_operations/basic_arithmetic.py
+++ practice_python_operations/basic_arithmetic.py
@@ -5,3 +5,9 @@ while start == "y":
     
 # asking which program
     program = int(input("Program(1-10)? "))
+
+    # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+    if program == 1:
+        num1 = int(input("1st number? "))
+        num2 = int(input("2nd number? "))
